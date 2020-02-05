8862a8b9f1c8f933ff64e5b8cb04a76a97ee59ac
RaZr0 <90razro@gmail.com>
2020-02-05 20:27:15 +0200

    accessibility header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,49 @@
-(function() {
-    const headerHamburger = document.querySelector('.header__hamburger');
-    const headerNav = document.querySelector('.header__nav');
-    headerHamburger.addEventListener('click', function() {
-        if (headerNav.classList.contains('open')) {
-            headerHamburger.src = '/images/icon-hamburger.svg';
-            headerNav.classList.remove('open');
-            document.body.classList.remove('mobile-nav-open');
-        } else {
-            headerHamburger.src = '/images/icon-close.svg';
-            headerNav.classList.add('open');
-            document.body.classList.add('mobile-nav-open');
+const insureApp = (function() {
+    // const headerHamburger = document.querySelector('.header__hamburger');
+    // const headerNav = document.querySelector('.header__nav');
+    // headerHamburger.addEventListener('click', function() {
+    //     if (headerNav.classList.contains('open')) {
+    //         headerHamburger.src = '/images/icon-hamburger.svg';
+    //         headerNav.classList.remove('open');
+    //         document.body.classList.remove('mobile-nav-open');
+    //     } else {
+    //         headerHamburger.src = '/images/icon-close.svg';
+    //         headerNav.classList.add('open');
+    //         document.body.classList.add('mobile-nav-open');
+    //     }
+    // });
+    return {
+        toggleMenu: function(navMenuButton) {
+            const headerHamburger = document.querySelector('.header__hamburger-img');
+            const headerNav = document.querySelector('.header__nav');
+            const headerItems = document.querySelector('.header__items');
+            if (headerNav.classList.contains('open')) {
+                closeMenu();
+            } else {
+                openMenu();
+            }
+
+            function closeMenu() {
+                headerHamburger.src = '/images/icon-hamburger.svg';
+                headerNav.classList.remove('open');
+                document.body.classList.remove('mobile-nav-open');
+                navMenuButton.focus();
+            }
+
+            function openMenu() {
+                headerHamburger.src = '/images/icon-close.svg';
+                headerNav.classList.add('open');
+                document.body.classList.add('mobile-nav-open');
+                headerItems.firstElementChild.firstElementChild.focus();
+                document.addEventListener('keyup', listenToMenuKeyStroke, event);
+            }
+
+            function listenToMenuKeyStroke($event) {
+                if ($event.keyCode === 27) {
+                    closeMenu();
+                    document.removeEventListener('keyup', listenToMenuKeyStroke);
+                }
+            }
         }
-    });
+    }
 })();
